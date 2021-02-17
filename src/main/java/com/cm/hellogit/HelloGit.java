834f20933163d4f485a8f8ec1e950c0d935ec322
ruizefirst <ruizefirst@163.com>
2021-02-18 02:16:55 +0800

    本地提交

---
+++ src/main/java/com/cm/hellogit/HelloGit.java
@@ -0,0 +1,13 @@
+package com.cm.hellogit;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("版本1");
+        System.out.println("创建分支");
+        System.out.println("主干新增");
+        System.out.println("分支新增");
+        System.out.println("在线更新");
+        System.out.println("在线修改项目名称");
+    }
+}
